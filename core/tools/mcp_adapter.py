247c9fd3d6895c8b217cbf20983d3cff1f1a7f41
JeelPatel231 <JeelPatel231@gmail.com>
2026-03-29 11:56:42 +0530

    implement actual tool calling

--- core/tools/mcp_adapter.py
+++ core/tools/mcp_adapter.py
@@ -2,7 +2,7 @@ from typing import Protocol, Self
 
 from core.tools import Tool, ToolResult
 from fastmcp import Client
-from mcp.types import Tool as McpTool
+from mcp.types import TextContent, Tool as McpTool
 from jsonschema import validate
 
 class McpToolAdapter(Protocol):
@@ -14,7 +14,7 @@ class McpToolAdapterImpl(McpToolAdapter):
         self.url = mcp_url
         self.client = Client(mcp_url)
 
-    def __create_tool(self, mcp_tool: McpTool):
+    def __create_tool(self, client: Client, mcp_tool: McpTool):
 
         class JsonSchemaToolArgs:
             def __init__(self, arguments: dict):
@@ -31,8 +31,12 @@ class McpToolAdapterImpl(McpToolAdapter):
 
         async def callback(args: JsonSchemaToolArgs) -> ToolResult:
             arguments = args.validated_args
-            print(f"Calling MCP tool {mcp_tool.name} with arguments: {arguments}")
-            raise NotImplementedError("Calling tools is not implemented yet.")
+            async with client:
+                result = await client.call_tool(mcp_tool.name, arguments)
+            
+            text_result = ''.join([x.text for x in result.content if isinstance(x, TextContent)])
+            # print(f"Calling MCP tool {mcp_tool.name} with arguments: {arguments}, got result: {result}")
+            return ToolResult(output=text_result)
 
         return Tool(
             name=mcp_tool.name,
@@ -41,9 +45,9 @@ class McpToolAdapterImpl(McpToolAdapter):
         )
 
     async def get_tools(self) -> list[Tool]:
-      async with self.client as client:
-          tools = await client.list_tools()
-          # resources = await client.list_resources()
-          # prompts = await client.list_prompts()
+        async with self.client as client:
+            tools = await client.list_tools()
+            # resources = await client.list_resources()
+            # prompts = await client.list_prompts()
 
-          return [self.__create_tool(tool) for tool in tools]
+        return [self.__create_tool(self.client, tool) for tool in tools]
